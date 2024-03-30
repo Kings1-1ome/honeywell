800e718f5871be29c9c74c94b69b385a78011c75
Kings1-1ome <165457691+Kings1-1ome@users.noreply.github.com>
2024-03-30 12:48:34 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormWithApi from './FormWithApi';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Form with API Integration</h1>
+      <FormWithApi />
+    </div>
+  );
+}
+
+export default App;
